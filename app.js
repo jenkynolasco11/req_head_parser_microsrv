9855fff5796262aebb5714c4f4ec195aa1dbccbe
Jenky W. Nolasco <jenky.nolasco@gmail.com>
2016-10-19 10:57:17 -0300

    finished request header parser

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
 var port = process.env.PORT;
 
-app.get('/',function(req,res){
+var http = require('http');
+
+var server = http.createServer(function(req,res){
   var header = req.headers;
-  // console.log(headers);
-  req.end(headers);
+    var os = header['user-agent'].match(/\(([\w\s.;_|]+)\)/)[1];
+    var ip = header['host'];
+    var lang = header['accept-language'].split(',')[0];
+    res.end(JSON.stringify({
+      ipaddress : ip,
+      language : lang,
+      software : os
+    }));
 });
 
-app.listen(port || 8001);
+// app.get('/',function(req,res){
+//   var header = req.headers;
+//   var os = header['user-agent'].match(/\(([\w\s.;_|]+)\)/)[1];
+//   var ip = header['host'];
+//   var lang = header['accept-language'].split(',')[0];
+//   res.end(JSON.stringify({
+//     ipaddress : ip,
+//     language : lang,
+//     software : os
+//   }));
+// });
+//
+// app.listen(port || 8001);
+server.listen(port || 8001);
